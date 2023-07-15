a560bdb95d93743f71f22432e26cdf3da40d1da4
Grigoris Kokozidis <kokozides.g@outlook.com>
2023-07-15 19:19:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Application started...");
+
+
+Solution solution = new Solution();
+
+int[] exampleArray = new int[] { 0, 4, 3, 0 };
+
+var target = 0;
+
+var result = solution.TwoSum(exampleArray, target);
+
+Console.WriteLine("[{0}]", string.Join(", ", result));
